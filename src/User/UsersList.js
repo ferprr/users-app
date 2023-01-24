e065c0064920256c09823010fcfb038f7537bd6b
Fernanda Pereira <FernandaPereira@iheartmedia.com>
2023-01-24 10:04:11 -0300

    mapping key for user

--- src/User/UsersList.js
+++ src/User/UsersList.js
@@ -2,13 +2,12 @@ import Card from "../UI/Card";
 import styles from './UsersList.module.css';
 import User from './User';
 
-//Renderiza usuários
 const UsersList = (props) => {
   return (
     <Card className={styles.users}>
       <ul>
         {props.users.map((user) => (
-          <User username={user.username} age={user.age}/>
+          <User key={user.id} username={user.username} age={user.age}/>
         ))}
       </ul>
     </Card>
